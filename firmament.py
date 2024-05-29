9731e2310d6b57f55d3f6870cc18270733594e38
NoFoolLikeOne <nofool@paparomeo.uk>
2024-05-29 15:05:05 +0000

    Batching queries and inserts

--- firmament.py
+++ firmament.py
@@ -1,9 +1,18 @@
+#!/bin/bash python3
 import pymysql
 import json
 import requests
 import os
 
 mysql_conn = None
+
+
+class SSDictCursor(pymysql.cursors.SSCursor, pymysql.cursors.DictCursorMixin):
+    """
+    A cursor that uses server-side cursors and returns rows as dictionaries.
+    """
+
+    pass
 
 
 def connect_database(database_secrets_file):
@@ -24,6 +33,7 @@ def connect_database(database_secrets_file):
                 secret.get("DB_PORT", 3306)
             ),  # Get the port from secrets, default to 3306 if not provided
             charset="utf8",  # Set the character set to UTF-8,
+            # cursorclass=SSDictCursor,
             cursorclass=pymysql.cursors.DictCursor,
         )
         return mysql_conn
@@ -37,96 +47,109 @@ def count_bodies(bodies):
     return bodyCount
 
 
-def system_current(id64):
+def fetch_systems(rows):
+    session = requests.Session()
+    headers = {"User-Agent": "Canonn firmament.py"}
+    systems = []
+
+    for row in rows:
+
+        url = f"https://spansh.co.uk/api/dump/{row.get('id64')}"
+        response = session.get(url, headers=headers)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json().get("system")
+            print(f"fetched {data.get('name')}")
+            systems.append(data)
+
+    session.close()
+    return systems
+
+
+missing_systems_query = """
+            SELECT DISTINCT CAST(id64 AS CHAR) AS id64 FROM codexreport cr where not exists
+            (select 1 from star_systems ss where ss.id64 = cr.id64)
+"""
+
+incomplete_systems_query = """
+            SELECT DISTINCT CAST(id64 AS CHAR) AS id64 FROM codexreport cr where not exists
+            (select 1 from star_systems ss where ss.id64 = cr.id64 and ifnull(ss.bodies_match,0) = 1)
+"""
+
+
+def insert_systems(systems):
     cursor = mysql_conn.cursor()
-    cursor.execute(
-        "SELECT bodies_match FROM star_systems WHERE id64 = %s and bodies_match = 1",
-        (id64,),
+    cursor.executemany(
+        "INSERT IGNORE INTO star_systems (raw_json) VALUES (%s) ON DUPLICATE KEY UPDATE raw_json = VALUES(raw_json)",
+        (systems),
     )
-    system = cursor.fetchmany()
+    print(f"{cursor.rowcount} systems out of {len(systems)} inserted.")
+
     cursor.close()
 
-    return cursor.rowcount
 
-
-def fetch_system(id64):
-    url = f"https://spansh.co.uk/api/dump/{id64}"
-    response = requests.get(url)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json().get("system")
-        return data
-
-
-def load_system(id64):
-
-    if system_current(id64) > 0:
-        print("System already loaded.")
-        return
-
-    data = fetch_system(id64)
-
-    # Check if the request was successful
-    if data:
-
-        # Extract the 'bodies' key from the data
-        bodies = data.pop("bodies", [])
-        # now we can replace with the count of bodies
-        data["lenBodies"] = count_bodies(bodies)
-        # Prepare JSON strings for insertion
-        star_system_json = json.dumps(data)
-
-        # Establish the database connection
-        cursor = mysql_conn.cursor()
-
-        # Insert the star system JSON
-        cursor.execute(
-            "INSERT IGNORE INTO star_systems (raw_json) VALUES (%s) ON DUPLICATE KEY UPDATE raw_json = VALUES(raw_json);",
-            (star_system_json,),
-        )
-        if cursor.rowcount == 0:
-            print(f"{data.get('name')} Already populated in database.")
-
-        # Insert each body JSON
-        values = []
-        for body in bodies:
-            body["systemAddress"] = data.get("id64")
-            body_json = json.dumps(body)
-            values.append(body_json)
-
-        cursor.executemany(
-            "INSERT IGNORE INTO system_bodies (raw_json) VALUES (%s) ON DUPLICATE KEY UPDATE raw_json = VALUES(raw_json);",
-            (values),
-        )
-        print(f"{cursor.rowcount} bodies out of {len(values)} inserted.")
-        cursor.close()
-        mysql_conn.commit()
-        print(
-            f"System {data.get('name')} and {data.get('lenBodies')} bodies loaded successfully."
-        )
-    else:
-        print("Failed to load system.")
-
-
-# create the path $HOME/.ssh/database_secrets.json using
-file_location = os.path.join(os.environ["HOME"], ".ssh", "database_secrets.json")
-
-mysql_conn = connect_database(file_location)
-
-with mysql_conn.cursor() as cursor:
-    # Execute the query
-    cursor.execute(
-        """
-        SELECT DISTINCT id64 FROM codexreport cr where not exists
-        (select 1 from star_systems ss where ss.id64 = cr.id64 and ss.bodies_match = 1)
-    """
+def insert_bodies(bodies):
+    cursor = mysql_conn.cursor()
+    cursor.executemany(
+        "INSERT IGNORE INTO system_bodies (raw_json) VALUES (%s) ON DUPLICATE KEY UPDATE raw_json = VALUES(raw_json)",
+        (bodies),
     )
+    print(f"{cursor.rowcount} bodies out of {len(bodies)} inserted.")
 
-    # Fetch all the distinct id64 values
-    # Fetch one row at a time
-    row = cursor.fetchone()
-    while row is not None:
-        load_system(row.get("id64"))
-        row = cursor.fetchone()
+    cursor.close()
+
+
+def process(query):
+    global mysql_conn
+
+    print("Processing query:", query)
+
+    cursor = mysql_conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute(
+        query,
+    )
+    print("Query executed.")
+    while True:
+        system_values = []
+        body_values = []
+        rows = cursor.fetchmany(1000)
+
+        if not rows:
+            break
+
+        print(f"Fetching {len(rows)} systems...")
+        system_data = fetch_systems(rows)
+        print(f"{len(system_data)} systems fetched.")
+
+        for data in system_data:
+
+            if data:
+                bodies = data.pop("bodies", [])
+                data["lenBodies"] = count_bodies(bodies)
+                for body in bodies:
+                    body["systemAddress"] = data.get("id64")
+                    body_json = json.dumps(body)
+                    body_values.append(body_json)
+                system_values.append(json.dumps(data))
+        insert_systems(system_values)
+        insert_bodies(body_values)
+        mysql_conn.commit()
+    # close the cursor we are done
+    cursor.close()
+
+
+def main():
+    # create the path $HOME/.ssh/database_secrets.json using
+    file_location = os.path.join(os.environ["HOME"], ".ssh", "database_secrets.json")
+    mysql_conn = connect_database(file_location)
+
+    # first we are going to process all the systems that are not in the database
+    # then we will work on systems that are out of date
+
+    process(missing_systems_query)
+    process(incomplete_systems_query)
+
+
+main()
